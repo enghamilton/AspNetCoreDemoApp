d3d13b01b57850e296464494af3295b342230e57
Hamilton Kamiya <31168921+enghamilton@users.noreply.github.com>
2019-11-21 14:48:09 -0300

    Update Startup.cs

--- src/AspNetCoreDemoApp/Startup.cs
+++ src/AspNetCoreDemoApp/Startup.cs
@@ -54,7 +54,6 @@ namespace AspNetCoreDemoApp
                     endpoints.MapDefaultControllerRoute();
                 });
             
-            ConnectionString = Configuration["ConnectionStrings:DefaultConnection"];
         }
     }
 }
