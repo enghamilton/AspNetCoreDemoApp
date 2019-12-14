079e1f4ff72dfa8e3eb41bf31d641632c4ccd79e
Hamilton Kamiya <31168921+enghamilton@users.noreply.github.com>
2019-12-14 19:56:30 -0300

    Update Startup.cs

--- src/AspNetCoreDemoApp/Startup.cs
+++ src/AspNetCoreDemoApp/Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AspNetCoreDemoApp
