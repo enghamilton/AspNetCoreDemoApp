cfcf6f28621878b88e7d9ce08816670fef6ed185
Hamilton Kamiya <31168921+enghamilton@users.noreply.github.com>
2019-12-17 06:36:39 -0300

    Create Product.cs

---
+++ src/AspNetCoreDemoApp/Model/Product.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AspNetCoreDemoApp.Model
+{
+    //[Table("tb_produto")]
+    public class Product
+    {
+        [Key]
+        public int id { get; set; }
+
+        public string name { get; set; }
+        public string phone { get; set; }
+        public string price { get; set; }
+    }
+}
