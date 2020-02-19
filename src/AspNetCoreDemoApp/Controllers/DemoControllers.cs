767524cbdd968b6ff320d46165c129d4ed0cec67
Hamilton Kamiya <31168921+enghamilton@users.noreply.github.com>
2020-02-19 00:31:54 -0300

    Update DemoControllers.cs

--- src/AspNetCoreDemoApp/Controllers/DemoControllers.cs
+++ src/AspNetCoreDemoApp/Controllers/DemoControllers.cs
@@ -6,24 +6,24 @@ using Microsoft.AspNetCore.Mvc;
 namespace AspNetCoreDemoApp.Controllers
 {
 	[Route("api/[controller]")]
-  public class DemoController : Controller
-{
-   [Route("")]
-   [Route("Demo")]
-   [Route("Demo/Index")]
-   public IActionResult Index()
-   {
-      return View("Views/Demo/index.cshtml");
-   }
-   [Route("Demo/About")]
-   public IActionResult About()
-   {
-      return View();
-   }
-   [Route("Demo/Contact")]
-   public IActionResult Contact()
-   {
-      return View();
-   }
-}
+	public class DemoController : Controller
+	{
+	   [Route("")]
+	   [Route("Demo")]
+	   [Route("Demo/Index")]
+	   public IActionResult Index()
+	   {
+		  return View("Views/Demo/index.cshtml");
+	   }
+	   [Route("Demo/About")]
+	   public IActionResult About()
+	   {
+		  return View();
+	   }
+	   [Route("Demo/Contact")]
+	   public IActionResult Contact()
+	   {
+		  return View();
+	   }
+	}
 }
